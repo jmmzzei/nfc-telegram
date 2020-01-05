ca8f0b2531770180ee3446501b570acee9a7c445
jmm <josemmazzei@gmail.com>
2020-01-05 16:02:42 -0300

    Modified index

--- index.js
+++ index.js
@@ -8,25 +8,24 @@ app.use(require('helmet')())
 const bot = new Telegraf(bot_token)
 
 app.set('port', process.env.PORT || port)
-bot.start((ctx) => ctx.reply('Welcome'))
+bot.start((ctx) => ctx.reply('Welcome human!'))
 
 app.use(bot.webhookCallback(`/${bot_token}`))
 bot.telegram.setWebhook(`https://telegram-nfc.herokuapp.com/${bot_token}`||`https://8a56230c.ngrok.io/${bot_token}`)
 
 app.get(`/${bot_token}`, async (req,res)=> {
     if (req.query.message) {
-        // send the message to a numberquery with bot.
         try {
-        await    bot.telegram.sendMessage(id, req.query.message)    
-        res.send('Enviado')
+            await bot.telegram.sendMessage(id, req.query.message)    
+            res.send('Message Sended!')
         } catch (error) {
             res.send(error)    
         }
     }else {
-        res.send('miss one param: either the auth code or the message')
+        res.send('No message to send')
     }
 })
 
 app.listen(app.get('port'), () => {
-    console.log(`server on port ${app.get('port')}`);
+    console.log(`Server on port ${app.get('port')}`);
 })
